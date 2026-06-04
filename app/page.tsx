4051c8fa48810dbee3fa21cf617624d9a14b23bb
Mac <mac@Vishesh-Device.local>
2026-06-04 15:01:16 +0530

    implement eslint and prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <h1 className='text-3xl text-violet-700 gont-black'>Welcome to nextjs!</h1>
+      <h1 className="text-3xl text-violet-700 gont-black">
+        Welcome to nextjs!
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,23 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import neostandard from "neostandard";
+import tailwindcss from "eslint-plugin-tailwindcss";
+import prettier from "eslint-config-prettier";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...neostandard(),
+  {
+    plugins: {
+      // tailwindcss,
+    },
+    extends: [
+      // "plugin:tailwindcss/recommended",
+      prettier,
+    ],
+  },
   // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
-
-export default eslintConfig;
